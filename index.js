3e337c4384ff4a1ab1d92ffbbc301b11cbd8cb13
Zack P <zack@zacklearns.com>
2017-10-12 15:43:47 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+import GitHub from "github.js";
+var ghdownload = require('github-download')
+
+gh = new GitHub();
+
+repo = gh.getRepositiory('kittenpkg','kitten');
+
+function kitten(install) {
+  ghdownload({user: 'kittenpkg', repo: 'kitten', ref: install}, process.cwd())
+};
