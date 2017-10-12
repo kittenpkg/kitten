8f66de053d24b667feea61d6334dde20c9b88301
Zack P <zack@zacklearns.com>
2017-10-12 12:24:40 -0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,10 @@
+{
+  "name": "kittenpkg",
+  "version": "1.0.0",
+  "description": "A reliable and safe package manager.",
+  "license": "MIT",
+  "main": "./bin/kitten.js"
+  "scripts": {
+    "test": "node ./scripts/test.js"
+  }
+}
