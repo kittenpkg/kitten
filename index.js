9be8e19ce771de45b04db5d5b52d93187ba3ed95
Zack P <zack@zacklearns.com>
2017-10-13 22:04:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 var GitHub = require('github.js')
-var ghdownload = require('github-download')
+var ghdownload = require('github-download-repo')
 
-gh = new GitHub();
+var gh = new GitHub();
 
-repo = gh.getRepositiory('kittenpkg','kitten');
+var args = process.argv.slice(2)
 
-function kitten(install) {
-  this.getDownload(package) {
-    ghdownload({user: 'kittenpkgs', repo: package, ref: 'master'}, process.cwd())
-  }
+function kitten() {
+  this.getDownload = function(package) {
+    ghdownload(gh.getRepository('kittenpkgs',package),'./node-modules')
+  };
+};
+
+if (args.length == 0) {
+  print('ERROR: No arguments found.')
+} elseif (args.length > 1) {
+  print('ERROR: Invalid arguments.')
+};
+
+if (args[0] == 'install') {
+  kitten().getDownload(args[1])
 };
